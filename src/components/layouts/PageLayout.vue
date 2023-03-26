5f4ae28e00c580d0264799e3d2d2c3e8e94987e5
Ivan Chavarro <ivan.chavarro@globant.com>
2023-03-26 12:22:49 -0500

    Adding a gap to alow scrolling on bottom part.

--- src/components/layouts/PageLayout.vue
+++ src/components/layouts/PageLayout.vue
@@ -5,7 +5,13 @@
     <div class="main">
       <slot name="body"></slot>
     </div>
+    <div class="gap"></div>
     <div class="footer">
       <slot name="footer"></slot>
     </div>
 </template>
+<style scoped>
+.gap {
+  height: 80px;
+}
+</style>
