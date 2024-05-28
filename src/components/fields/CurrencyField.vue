bb8b5facf4f3304fd6a8a3af0c75881cb6a0a385
Ivan Chavarro <ivan.chavarro@globant.com>
2024-05-28 18:23:47 -0500

    Adding cost and number of people.

---
+++ src/components/fields/CurrencyField.vue
@@ -0,0 +1,77 @@
+<template>
+<div class="text-left my-4">
+    <label :for="inputId" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+      {{ label }}
+    </label>
+    <input :type="type" :name="name" :id="inputId"
+      ref="inputRef"
+      :value="modelValue"
+      :class="classes"
+    />
+    <div class="msg" v-if="msg && msg.type">
+      <p v-if="msg.type === 'error'" class="mt-2 text-sm text-red-600 dark:text-red-500">{{ msg.text }}</p>
+      <p v-if="msg.type === 'success'" class="mt-2 text-sm text-green-600 dark:text-green-500">{{ msg.text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useCurrencyInput } from 'vue-currency-input'
+
+export default {
+  props: {
+    modelValue: Number,
+    options: Object,
+    label: String,
+    inputId: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    name: String,
+    msg: Object,
+  },
+  setup(props) {
+    const { inputRef } = useCurrencyInput(props.options)
+
+    return { inputRef }
+  },
+  computed: {
+    classes() {
+      return {
+        'border': true,
+        'bg-gray-50': !this.msg,
+        'border-gray-300': !this.msg,
+        'text-gray-900': !this.msg,
+        'focus:ring-blue-500': !this.msg,
+        'focus:border-blue-500': !this.msg,
+        'dark:bg-gray-700': true,
+        'dark:border-gray-600': !this.msg,
+        'dark:placeholder-gray-400': !this.msg,
+        'dark:text-white': true,
+        'dark:focus:ring-blue-500': true,
+        'dark:focus:border-blue-500': true,
+        'block': true,
+        'w-full': true,
+        'p-2.5': true,
+        'text-sm': true,
+        'rounded-lg': true,
+        'bg-red-50': this.msg?.type && this.msg.type === 'error',
+        'border-red-500': this.msg?.type && this.msg.type === 'error',
+        'text-red-900': this.msg?.type && this.msg.type === 'error',
+        'placeholder-red-700': this.msg?.type && this.msg.type === 'error',
+        'focus:ring-red-500': this.msg?.type && this.msg.type === 'error',
+        'focus:border-red-500': this.msg?.type && this.msg.type === 'error',
+        'dark:text-red-500': this.msg?.type && this.msg.type === 'error',
+        'dark:placeholder-red-500': this.msg?.type && this.msg.type === 'error',
+        'dark:border-red-500': this.msg?.type && this.msg.type === 'error',
+
+      }
+    }
+  }
+};
+
+</script>
--- src/views/reservations/ReservationView.vue
+++ src/views/reservations/ReservationView.vue
@@ -1,116 +1,187 @@
 <template>
-   <div v-if="$route.name === 'reservation-delete'">
+  <div v-if="$route.name === 'reservation-delete'">
     <h2>¿Esta seguro de eliminar esta reserva?</h2>
     <button
       v-on:click="deleteItem"
-      class="bg-red-600 text-white font-bold py-2 px-4 rounded">
+      class="bg-red-600 text-white font-bold py-2 px-4 rounded"
+    >
       Eliminar
     </button>
-    <router-link :to="{ name: 'items' }" >
-      Cancelar
-    </router-link>
+    <router-link :to="{ name: 'items' }"> Cancelar </router-link>
   </div>
   <div v-else>
     <h1 class="mt-4 p-2 text-xl font-bold">Reserva</h1>
 
-    <div :class="{'rounded-lg': true,
+    <div
+      :class="{
+        'rounded-lg': true,
         'p-2': true,
         'bg-red-50': errors?.date,
-        'border-red-500': errors?.date}">
-      <VueDatePicker v-model="reservation.date" range format="Y-m-d"></VueDatePicker>
+        'border-red-500': errors?.date,
+      }"
+    >
+      <VueDatePicker
+        v-model="reservation.date"
+        range
+        format="Y-m-d"
+      ></VueDatePicker>
       <p
         v-if="errors?.date"
-        class="mt-2 text-sm text-red-600 dark:text-red-500">
+        class="mt-2 text-sm text-red-600 dark:text-red-500"
+      >
         {{ errors.date.text }}
       </p>
     </div>
 
-    <text-area-field inputId="contact" name="contact" label="Información de Contacto" v-model="reservation.contact"></text-area-field>
+    <text-area-field
+      inputId="contact"
+      name="contact"
+      label="Información de Contacto"
+      v-model="reservation.contact"
+    ></text-area-field>
 
-    <div :class="{'rounded-lg': true,
+    <div class="text-left my-4">
+      <label for="quantity" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Número de personas
+      </label>
+      <select
+        name="quantity"
+        id="quantity"
+        v-model="reservation.quantity"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 mb-4 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        <option
+          v-for="i in 8"
+          :value="i"
+          :key="i"
+          :selected="reservation.quantity === i"
+        >
+          {{ i < 10 ? `0${i}` : i }}
+        </option>
+      </select>
+    </div>
+
+    <CurrencyField
+      name="cost"
+      label="Pago"
+      v-model="reservation.cost"
+      inputId="cost"
+      :options="{ currency: 'COP', narrowSymbol: true, code: 'co', symbol: '$' }"
+    />
+
+    <div
+      :class="{
+        'rounded-lg': true,
         'p-4': true,
-        'mb-4':true,
+        'mb-4': true,
         'bg-red-50': this.errors?.type,
-        'border-red-500': this.errors?.type}">
+        'border-red-500': this.errors?.type,
+      }"
+    >
       <div class="text-left my-4">
-        <input type="radio" name="role" id="user-status-1" class="mr-2" value="airbnb" v-model="reservation.type"/>
+        <input
+          type="radio"
+          name="role"
+          id="user-status-1"
+          class="mr-2"
+          value="airbnb"
+          v-model="reservation.type"
+        />
         <label for="user-status-1" class="font-bold w-full mr-2">AirBnB</label>
       </div>
       <div class="text-left my-4">
-        <input type="radio" name="role" id="user-status-2" class="mr-2" value="booking" v-model="reservation.type"/>
+        <input
+          type="radio"
+          name="role"
+          id="user-status-2"
+          class="mr-2"
+          value="booking"
+          v-model="reservation.type"
+        />
         <label for="user-status-2" class="font-bold w-full">Booking</label>
       </div>
       <p
         v-if="errors?.type"
-        class="mt-2 text-sm text-red-600 dark:text-red-500">
+        class="mt-2 text-sm text-red-600 dark:text-red-500"
+      >
         {{ errors.type.text }}
       </p>
     </div>
 
     <div>
-      Agregar clave?
-      <user-number-lock :active="true" v-model="reservation.userLock"></user-number-lock>
+      ¿Agregar clave?
+      <user-number-lock
+        :active="true"
+        v-model="reservation.userLock"
+      ></user-number-lock>
     </div>
 
     <ul class="grid grid-cols-3 gap-2 mb-4 items-center">
-      <li class="p-2 rounded-md border text-xs"
-        v-for="item in reservation.items" v-bind:key="item._id">
+      <li
+        class="p-2 rounded-md border text-xs"
+        v-for="item in reservation.items"
+        v-bind:key="item._id"
+      >
         {{ item.label }}
       </li>
     </ul>
-    <button v-on:click="save" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Guardar</button>
-    <router-link v-if="id"
-      :to="{ name: 'reservation-delete', id: reservation.id }" class="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+    <button
+      v-on:click="save"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Guardar
+    </button>
+    <router-link
+      v-if="id"
+      :to="{ name: 'reservation-delete', id: reservation.id }"
+      class="ml-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+    >
       Eliminar
     </router-link>
   </div>
 </template>
 <script>
-import TextAreaField from '@/components/fields/TextAreaField'
-import VueDatePicker from '@vuepic/vue-datepicker'
-import '@vuepic/vue-datepicker/dist/main.css'
-import UserNumberLock from '@/components/atoms/UserNumberLock.vue'
+import TextAreaField from "@/components/fields/TextAreaField";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import UserNumberLock from "@/components/atoms/UserNumberLock.vue";
+import CurrencyField from "@/components/fields/CurrencyField.vue";
+import "@vuepic/vue-datepicker/dist/main.css";
 
 export default {
-  components: { TextAreaField, VueDatePicker, UserNumberLock },
-  props: ['id'],
+  components: { TextAreaField, CurrencyField, VueDatePicker, UserNumberLock },
+  props: ["id"],
   data() {
     return {
       reservation: {
         items: {},
-        date: []
+        date: [],
+        cost: 0,
       },
       errors: {},
       validations: [
         {
-          field: 'date',
-          rules: [
-            { required: true }
-          ]
+          field: "date",
+          rules: [{ required: true }],
         },
         {
-          field: 'type',
+          field: "type",
           rules: [
             {
               required: true,
-              rewriteMsg: 'Debes seleccionar una'
-            }
-          ]
-        }
-      ]
-    }
+              rewriteMsg: "Debes seleccionar una",
+            },
+          ],
+        },
+      ],
+    };
   },
   computed: {
     dateRange() {
-      return [
-        this.reservation.dateIni,
-        this.reservation.dateEnd
-      ]
-    }
+      return [this.reservation.dateIni, this.reservation.dateEnd];
+    },
   },
   methods: {
     async save() {
-
       this.errors = this.validateForm(this.validations, this.reservation);
       if (Object.keys(this.errors).length > 0) {
         return false;
@@ -122,11 +193,11 @@ export default {
 
       this.reservation.validated = false;
 
-      if (this.$route.name === 'reservation-edit') {
+      if (this.$route.name === "reservation-edit") {
         try {
           const { error } = await this.callEndpoints(
             this.axios,
-            'put',
+            "put",
             `/reservations/${this.id}`,
             localStorage.token,
             this.reservation
@@ -134,10 +205,8 @@ export default {
 
           if (error) {
             console.log(error);
-          }
-
-          else {
-            this.$router.push({ name: 'reservations' });
+          } else {
+            this.$router.push({ name: "reservations" });
           }
         } catch (error) {
           console.log(error);
@@ -145,8 +214,8 @@ export default {
       } else {
         const { error } = await this.callEndpoints(
           this.axios,
-          'post',
-          '/reservations/',
+          "post",
+          "/reservations/",
           localStorage.token,
           this.reservation
         );
@@ -154,15 +223,16 @@ export default {
         if (error) {
           console.log(error);
         } else {
-          this.$router.push({ name: 'reservations' });
+          this.$router.push({ name: "reservations" });
         }
       }
     },
     async deleteItem() {
       const response = await this.callEndpoints(
         this.axios,
-        'delete',
-        `/reservations/${this.id}`, localStorage.token
+        "delete",
+        `/reservations/${this.id}`,
+        localStorage.token
       );
 
       if (response?.error) {
@@ -170,33 +240,34 @@ export default {
           text: response.error.displayMsg,
           type: response.error.type,
           show: true,
-        }
-      }
-      else {
+        };
+      } else {
         this.message = {
-          text: response?.data?.deleted  === true ? 'Item eliminado correctamente.' : 'Ocurrio un error',
-          type: 'success',
+          text:
+            response?.data?.deleted === true
+              ? "Item eliminado correctamente."
+              : "Ocurrio un error",
+          type: "success",
           show: true,
-        }
+        };
       }
 
-      this.$router.push({ name: 'reservations' });
+      this.$router.push({ name: "reservations" });
     },
   },
   async mounted() {
-    if (this.$route.name === 'reservation-new') {
+    if (this.$route.name === "reservation-new") {
       try {
         const response = await this.callEndpoints(
           this.axios,
-          'get',
-          '/items/all?limit=10&offset=0',
+          "get",
+          "/items/all?limit=10&offset=0",
           localStorage.token
         );
 
         if (response.error) {
           console.log(response);
-        }
-        else {
+        } else {
           this.reservation.items = response.data;
         }
       } catch (error) {
@@ -206,26 +277,24 @@ export default {
       try {
         const response = await this.callEndpoints(
           this.axios,
-          'get',
+          "get",
           `/reservations/${this.id}`,
           localStorage.token
         );
 
         if (response.error) {
           console.log(response);
-        }
-        else {
+        } else {
           this.reservation = response.data;
           this.reservation.date = [
             this.reservation.dateIni,
-            this.reservation.dateEnd
-          ]
+            this.reservation.dateEnd,
+          ];
         }
-
       } catch (error) {
         console.log(error);
       }
     }
-  }
-}
+  },
+};
 </script>
